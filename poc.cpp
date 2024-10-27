d6abb583d00c7494aae8dfe44158bdd47f4e1a5a
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-27 12:44:14 +0000

    drawing a quad

--- poc.cpp
+++ poc.cpp
@@ -3,18 +3,21 @@
 #pragma leco add_shader "poc.vert"
 
 import casein;
+import vee;
 import voo;
 
 struct thread : voo::casein_thread {
   void run() override {
     voo::device_and_queue dq { "poc" };
 
+    auto pl = vee::create_pipeline_layout();
+    voo::one_quad_render oqr { "poc", &dq, *pl };
+
     while (!interrupted()) {
       voo::swapchain_and_stuff sw { dq };
 
-      extent_loop(dq.queue(), sw, [&] {
-        sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
-        });
+      ots_loop(dq, sw, [&](auto cb) {
+        oqr.run(cb, sw.extent());
       });
     }
   }
--- poc.vert
+++ poc.vert
@@ -3,6 +3,6 @@
 layout(location = 0) in vec2 pos;
 
 void main() {
-  gl_Position = vec4(pos, 0, 1);
+  gl_Position = vec4(pos * 2.0 - 1.0, 0, 1);
 }
  
