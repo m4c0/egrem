e4280549df17812565cea1f2e9f535cb1ec70490
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-28 10:44:50 +0000

    aspect on shader

--- poc.cpp
+++ poc.cpp
@@ -6,17 +6,27 @@ import casein;
 import vee;
 import voo;
 
+struct upc {
+  float aspect;
+};
+
 struct thread : voo::casein_thread {
   void run() override {
     voo::device_and_queue dq { "poc" };
 
-    auto pl = vee::create_pipeline_layout();
+    auto pl = vee::create_pipeline_layout({
+      vee::vert_frag_push_constant_range<upc>()
+    });
     voo::one_quad_render oqr { "poc", &dq, *pl };
+
+    upc pc {};
 
     while (!interrupted()) {
       voo::swapchain_and_stuff sw { dq };
+      pc.aspect = sw.aspect();
 
       ots_loop(dq, sw, [&](auto cb) {
+        vee::cmd_push_vert_frag_constants(cb, *pl, &pc);
         oqr.run(cb, sw.extent());
       });
     }
--- poc.vert
+++ poc.vert
@@ -1,5 +1,9 @@
 #version 450
 
+layout(push_constant) uniform upc {
+  float aspect;
+} pc;
+
 layout(location = 0) in vec2 pos;
 
 layout(location = 0) out vec2 frag_pos;
@@ -8,6 +12,6 @@ void main() {
   vec2 p = pos * 2.0 - 1.0;
   gl_Position = vec4(p, 0, 1);
 
-  frag_pos = p;
+  frag_pos = vec2(p.x * pc.aspect, p.y);
 }
  
